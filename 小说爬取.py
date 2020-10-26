a867adf0b837c25207515573e40f939860f23635
billy-db <52339049+billy-db@users.noreply.github.com>
2020-10-27 06:36:31 +0800

    Update 小说爬取.py

--- 小说爬取.py
+++ 小说爬取.py
@@ -14,19 +14,30 @@ page_text = page.text
 #在首页中解析出章节的的标题和详情页的url
 soup = BeautifulSoup(page_text,'lxml')
 #print(soup)
-fp = open('./111.txt','w',encoding='utf-8',errors='igore')
+fp = open('./abc.txt','w',encoding='utf-8',errors='igore')
 a_list = soup.select('#list-chapterAll > .panel-body > dd > a')
 #print(a_list)
 for i in a_list:
     b = i.string
-    print(b)
+    #print(b)
+    a  = i['href']
+    a = a.split('.')[0]+'_2.html'
+    #print(a)
     detail_url = url+i['href']
-    print(detail_url)
+    detail_url2 = url+a
+    #print(detail_url+'\t'+detail_url2 )
     detail = requests.get(url=detail_url,headers=headers)
+    detail2 = requests.get(url=detail_url2,headers=headers)
     detail.encoding = 'gbk'
+    detail2.encoding = 'gbk'
     detail_text = detail.text
+    detail2_text = detail2.text
     detail_soup = BeautifulSoup(detail_text,'lxml')
+    detail_soup2 = BeautifulSoup(detail2_text,'lxml')
     div_tag = detail_soup.find('div',class_='panel-body')
+    div_tag2 = detail_soup2.find('div',class_='panel-body')
     content = div_tag.text
-    fp.write(b+':'+content+'\n')
+    content2 = div_tag2.text
+    fp.write(b+':'+content+content2+'\t')
+    #fp.write(content2+'\t')
     print(b,"爬取成功")
